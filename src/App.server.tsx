eac7fdf6153db5fecd662a0132eb05e71f8d7013
CaptainCoder <78704276+captainplanto@users.noreply.github.com>
2022-10-30 15:27:26 +0100

    inital package lock settings

--- src/App.server.tsx
+++ src/App.server.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import renderHydrogen from '@shopify/hydrogen/entry-server';
 import {Router, FileRoutes, ShopifyProvider} from '@shopify/hydrogen';
 import {Suspense} from 'react';
